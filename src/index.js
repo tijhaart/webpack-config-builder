860da050a4fbba36a1c291436a30b6ba54029c94
Raymond Tijhaar <contact@raymond.tijhaar.com>
2016-06-06 21:36:42 +0200

    Rewrite and removed code for 0.1.0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,80 @@
-import Config, {exportToWebpackConfig} from './config';
-export default Config;
-export {
-  exportToWebpackConfig
-};
+import Debug from 'debug';
+import { fromJS, Map } from 'immutable';
+import fp from 'lodash/fp';
+
+const error = Debug('webpack-config-builder:error');
+const debug = Debug('webpack-config-builder:debug');
+
+const defaultReducers = [c => c];
+
+/**
+ * @type Config
+ * @param {Object|Immutable.Map} state
+ * @param {Function[]} reducers
+ */
+export default function Config(state, reducers = defaultReducers) {
+  state = Map.isMap(state) ? state : fromJS(state);
+
+  return {
+    use: use,
+    useIf: useIf,
+    toJs: toJs
+  };
+
+  /**
+   * toJs
+   * @returns {Object|Boolean}
+   */
+  function toJs() {
+    try {
+      return fp.flow(reducers)(state).toJS();
+    } catch(err) {
+      error(err);
+
+      return false;
+    }
+  }
+
+  /**
+   * Use
+   * @param {Function} reducer - function that receives the state object
+   * @returns {Config}
+   */
+  function use(reducer) {
+    debug(`Added reducer: '${reducer.name || 'Anonymous'}'`);
+    return Config(state, reducers.concat(reducer));
+  }
+
+  /**
+   * Run reducer only if condition in config is met
+   * @param {String[]} getInPath - Path in configurator to value
+   * @param {Function} reducer   - state 'changer'
+   * @returns {Config}
+   */
+  function useIf(getInPath, reducer) {
+    return this.use(c => {
+      if (c.getIn(getInPath)) {
+        return reducer(c);
+      }
+      return c;
+    });
+  }
+}
+
+/**
+ * exportForWebpack
+ * @note exportForWebpack doesn't cover all webpack settings that require mapping from object to array.
+ */
+export function exportForWebpack(c) {
+  return c.withMutations(c => {
+    return c
+      .updateIn(['module', 'loaders'], objectToArray)
+      .updateIn(['plugins'], objectToArray)
+      .updateIn(['resolve', 'modulesDirectories'], objectToArray)
+      .updateIn(['externals'], objectToArray);
+  });
+}
+
+function objectToArray(obj) {
+  return obj ? obj.toArray(): [];
+}
---
+++ webpack.config.babel.js
@@ -0,0 +1,74 @@
+import Config, { exportForWebpack } from './src/index.js';
+import pkg from './package.json';
+import nodeExternals from 'webpack-node-externals';
+import fp from 'lodash/fp';
+
+let cfg = Config({
+  env: getNodeEnv(),
+  context: __dirname + '/src',
+  debug: true,
+  cache: true,
+  watch: true,
+  devtool: '#cheap-module-eval-inline-source-map',
+  entry: {
+    main: './index.js',
+  },
+  output: {
+    path: './lib',
+    filename: `${pkg.name}.js`
+  },
+  resolve: {
+    root: void(0),
+    modulesDirectories: {
+      'default': __dirname + '/node_modules'
+    }
+  },
+  module: {
+    loaders: {}
+  },
+  plugins: {},
+})
+.useIf(['env', 'production'], c => c.set('devtool', void(0)))
+.useIf(['env', 'production'], c => c.set('debug', false).set('watch', false))
+.use(babelConfig())
+.use(nodeWebpack)
+;
+
+cfg = cfg
+.use(exportForWebpack);
+
+getNodeEnv().development && console.log(cfg.toJs());
+
+export default cfg.toJs();
+
+function getNodeEnv() {
+  const { NODE_ENV } = process.env;
+
+  return {
+    development: NODE_ENV === 'development' || (NODE_ENV !== 'production' && NODE_ENV !== 'test'),
+    production: NODE_ENV === 'production',
+    test: NODE_ENV === 'test'
+  };
+}
+
+function nodeWebpack(c) {
+  return c
+    .set('target', 'node')
+    .setIn(['externals', 'node'], nodeExternals())
+  ;
+}
+
+function babelConfig(loader, options={loaderKey:'babel'}) {
+  loader = fp.defaultsDeep({
+    test: /\.js$/,
+    loader: 'babel',
+    exclude: /(node_modules|bower_components)/,
+    query: {
+      presets: ['es2015']
+    }
+  })(loader);
+
+  return (c) => {
+    return c.setIn(['module', 'loaders', options.loaderKey], loader);
+  };
+}
